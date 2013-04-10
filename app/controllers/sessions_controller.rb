773d353324ba11cef70159f9e85faab812e10caf
Digital-Learner <digital.learning.journeys@gmail.com>
2013-04-10 18:30:06 +0100

    make omniauth facebook sign in work

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
 class SessionsController < ApplicationController
   def create
-    render json: request.env['omniauth.auth']
+    user = User.from_omniauth(env["omniauth.auth"])
+    session[:user_id] = user.id
+    redirect_to root_url
   end
 end
