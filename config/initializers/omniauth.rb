67c0811495ad2060d4c89eecda4cd027f113a190
Digital-Learner <digital.learning.journeys@gmail.com>
2013-04-10 22:57:55 +0100

    Start on Twitter omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,4 +3,5 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :developer unless Rails.env.production?
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
 end
