ea06002fe5f350f8e8282e5efc46d6211107bdec
Michelle <yhzgirl@gmail.com>
2013-05-02 18:00:38 +0100

    refactor new response to render form

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,8 +1,11 @@
 class Response < ActiveRecord::Base
+  
   attr_accessible :correct, :question_id, :user_id, :user_response
 
   belongs_to :user
   belongs_to :question
+
+  # validates :user_response, presence: true
   
   def is_correct?
     user_response == Question.find(question_id).fact_or_fiction
