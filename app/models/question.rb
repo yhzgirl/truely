94418c35b7c4c832c6b364ff5887171bd2f07967
Michelle <yhzgirl@gmail.com>
2013-05-05 14:47:56 +0100

    Ensure 'fact_or_fiction' has a value

--- app/models/question.rb
+++ app/models/question.rb
@@ -10,6 +10,7 @@ class Question < ActiveRecord::Base
   validates :statement, presence: true
   validates :statement, length: { minimum: 5, maximum: 255 }
   validates :statement, uniqueness: { case_sensitive: true }
+  validates :fact_or_fiction, presence: true
 
   # Used by will_paginate to set no. users per page
   self.per_page = 15
