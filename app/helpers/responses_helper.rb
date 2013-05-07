04c793efa21d8685a9083a02729d7fc948f505ad
Michelle <yhzgirl@gmail.com>
2013-05-07 20:16:45 +0100

    Display users %age correctly answered

--- app/helpers/responses_helper.rb
+++ app/helpers/responses_helper.rb
@@ -2,4 +2,11 @@ module ResponsesHelper
   def question_response_right_or_wrong
     @response.correct == true ? "right" : "wrong"
   end
+
+  def response_statistics
+    @user_stat = Response.percentage_correctly_answered(current_user)
+    sprintf('%.0f', @user_stat)
+  end
+
+  
 end
