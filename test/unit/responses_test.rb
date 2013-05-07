54081deeda72d7de310254f14310ab2bab491c48
Michelle <yhzgirl@gmail.com>
2013-05-07 19:32:46 +0100

    refactor ResponseTest

--- test/unit/responses_test.rb
+++ test/unit/responses_test.rb
@@ -4,37 +4,32 @@ require 'factories/response_factory'
 
 class ResponseTest < ActiveSupport::TestCase
 
+  def setup
+    @user = User.create!(name: 'bob')
+    response = ResponseFactory.correct_response(:user_id => @user.id)
+    response2 = ResponseFactory.correct_response(:user_id => @user.id)
+    response3 = ResponseFactory.incorrect_response(:user_id => @user.id)
+    response4 = ResponseFactory.incorrect_response(:user_id => @user.id)
+  end
+
   test "correct if user response matches question's set fact or fiction value" do
     user_response = ResponseFactory.correct_response
     assert_equal true, user_response.is_correct?
   end
 
   test 'can determine number of statements user has answered' do    
-    user = User.create!(name: 'bob')
-    response = ResponseFactory.correct_response(:user_id => user.id)
-    response2 = ResponseFactory.correct_response(:user_id => user.id)
-    assert_equal 2, Response.count  
-    user_has_answered = Response.num_questions_answered(user)
-    assert_equal 2, user_has_answered
+    assert_equal 4, Response.count  
+    user_has_answered = Response.num_questions_answered(@user)
+    assert_equal 4, user_has_answered
   end
 
   test 'can determine number of responses which are correct' do
-    user = User.create!(name: 'bob')
-    response = ResponseFactory.correct_response(:user_id => user.id)
-    response2 = ResponseFactory.correct_response(:user_id => user.id)
-    response3 = ResponseFactory.incorrect_response(:user_id => user.id)
-    response4 = ResponseFactory.incorrect_response(:user_id => user.id)
-    correctly_answered = Response.num_correctly_answered(user)
+    correctly_answered = Response.num_correctly_answered(@user)
     assert_equal 2, correctly_answered
   end
 
   test 'calculate percentage of correctly answered questions' do
-    user = User.create!(name: 'bob')
-    response = ResponseFactory.correct_response(:user_id => user.id)
-    response2 = ResponseFactory.correct_response(:user_id => user.id)
-    response3 = ResponseFactory.incorrect_response(:user_id => user.id)
-    response4 = ResponseFactory.incorrect_response(:user_id => user.id)
-    percentage_correctly_answered = Response.percentage_correctly_answered(user)
+    percentage_correctly_answered = Response.percentage_correctly_answered(@user)
     assert_equal 50, percentage_correctly_answered   
   end
 
